c702b9aec77312556941ecf693ae4524c6316720
No-detka <no-detka@mail.ru>
2025-02-25 14:43:01 +0300

    Add files via upload

---
+++ src/components/UI/Loader/Loader.tsx
@@ -0,0 +1,23 @@
+import { FunctionComponent } from 'react';
+import clsx from 'clsx';
+
+import styles from './Loader.module.scss';
+
+interface LoaderProps {
+  bigLoad?: boolean;
+}
+
+const Loader: FunctionComponent<LoaderProps> = ({ bigLoad = false }) => {
+  return (
+    <div className={styles.loader}>
+      <img
+        src="./loaderIcon.svg"
+        alt="Loader"
+        className={clsx(styles.loaderIcon, { [styles.loaderIconBig]: bigLoad })}
+      />
+      {bigLoad && <p className={styles.text}>Загружаем данные</p>}
+    </div>
+  );
+};
+
+export default Loader;
